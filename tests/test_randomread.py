5ef539f01d57e5591cb311bc124cd64c90409548
ninpnin <vainoyrjanainen@icloud.com>
2023-04-14 14:38:37 +0200

    test: the read text should be different each time

--- tests/test_randomread.py
+++ tests/test_randomread.py
@@ -11,6 +11,11 @@ class Test(unittest.TestCase):
         text = sample("tests/data.txt", 1009, chunk_size=1111)
         self.assertEqual(len(text.split()), 1009)
 
+    def test_different(self):
+        text1 = sample("tests/data.txt", 100, chunk_size=100)
+        text2 = sample("tests/data.txt", 100, chunk_size=100)
+        self.assertNotEqual(text1, text2)
+
 
 if __name__ == '__main__':
     # begin the unittest.main()
