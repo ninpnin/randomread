791fa1a1f4b06b8f61cd6469c78106f3fc13d043
ninpnin <vainoyrjanainen@icloud.com>
2023-04-14 14:26:16 +0200

    test: basic functionality

--- tests/test_randomread.py
+++ tests/test_randomread.py
@@ -1,5 +1,17 @@
-from randomread import __version__
+from randomread import sample
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_small(self):
+        text = sample("tests/data.txt", 10)
+        self.assertEqual(len(text.split()), 10)
+
+    def test_large(self):
+        text = sample("tests/data.txt", 1009, chunk_size=1111)
+        self.assertEqual(len(text.split()), 1009)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
