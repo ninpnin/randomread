5205ad4bf7d5a425b9fce68a4696aac5c000f975
ninpnin <vainoyrjanainen@icloud.com>
2023-04-14 14:49:33 +0200

    test: match the test cases with the README examples

--- tests/test_randomread.py
+++ tests/test_randomread.py
@@ -8,12 +8,12 @@ class Test(unittest.TestCase):
         self.assertEqual(len(text.split()), 10)
 
     def test_large(self):
-        text = sample("tests/data.txt", 1009, chunk_size=1111)
-        self.assertEqual(len(text.split()), 1009)
+        text = sample("tests/data.txt", 200000, chunk_size=2000)
+        self.assertEqual(len(text.split()), 200000)
 
     def test_different(self):
-        text1 = sample("tests/data.txt", 100, chunk_size=100)
-        text2 = sample("tests/data.txt", 100, chunk_size=100)
+        text1 = sample("tests/data.txt", 1000, chunk_size=100)
+        text2 = sample("tests/data.txt", 1000, chunk_size=100)
         self.assertNotEqual(text1, text2)
 
 
